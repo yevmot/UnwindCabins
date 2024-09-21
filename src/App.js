1fb3634740680f9a612043d3f57585c403ee704c
Eugene Motrych <motrihzenya@gmail.com>
2024-09-21 15:20:33 +0300

    created form search, calendar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import Discover from "./components/Discover/Discover";
 import Cards from "./components/cards/Cards";
+import Search from "./components/search/Search";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <Header />
+            <Search />
             <Discover />
             <Cards />
         </div>
---
+++ src/components/search/button/FormBtn.js
@@ -0,0 +1,9 @@
+const FormBtn = () => {
+    return (
+        <div className="form-btn">
+            <button type="button">Find available cabins</button>
+        </div>
+    );
+};
+
+export default FormBtn;
